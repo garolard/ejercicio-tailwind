ec59e929d87e8e329198fc4b6767970e9ed2a3d7
Gabriel Ferreiro <gbril9119@gmail.com>
2021-01-29 13:52:00 +0100

    Ejercicio de layout y aprendizaje de TailwindCSS. Llegamos hasta la primera tarjetilla

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [
+    './src/**/*.tsx',
+    './src/**/*.ts',
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+  ],
+}
